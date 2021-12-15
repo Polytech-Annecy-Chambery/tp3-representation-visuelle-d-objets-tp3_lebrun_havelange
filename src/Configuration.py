7b97327cbf03b3f3b00c8bcc8c652a4089e2ccfd
VictorMathilde <95290133+VictorMathilde@users.noreply.github.com>
2021-12-15 13:47:24 +0100

    Add files via upload

--- src/Configuration.py
+++ src/Configuration.py
@@ -74,7 +74,8 @@ class Configuration:
 
         gl.glMatrixMode(gl.GL_MODELVIEW)
         gl.glLoadIdentity()
-        gl.glTranslatef(0.0,0.0, self.parameters['screenPosition'])       
+        gl.glTranslatef(0.0,0.0, self.parameters['screenPosition']) 
+        gl.glRotate(90,1,0,0)
         
     # Getter
     def getParameter(self, parameterKey):
@@ -149,11 +150,21 @@ class Configuration:
     
     # Processes the MOUSEBUTTONDOWN event
     def processMouseButtonDownEvent(self):
-        pass
+        if self.event.button == 4:
+            gl.glScalef(1.1,1.1,1.1)
+        elif self.event.button == 5:
+            gl.glScalef(1/1.1,1/1.1,1/1.1)
+            
     
     # Processes the MOUSEMOTION event
     def processMouseMotionEvent(self):
-        pass
+        if pygame.mouse.get_pressed()[0] == 1:
+            gl.glRotate(self.event.rel[0],1,0,0)
+            gl.glRotate(self.event.rel[1],0,0,1)
+        elif pygame.mouse.get_pressed()[2] == 1:
+            gl.glTranslate((self.event.rel[0]/20),0,0)
+            gl.glTranslate(0,0,self.event.rel[1]/20)
+        
          
     # Displays on screen and processes events    
     def display(self): 
